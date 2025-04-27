9647391f61325b9c6081382d6afdc551fdbb8b8d
Danelle Lavern <danellel@ne-e3.nn.21-school.ru>
2025-04-27 16:03:44 +0300

    parse_flag

--- cat/s21_cat.c
+++ cat/s21_cat.c
@@ -4,6 +4,7 @@
 int main(int argc, char** argv) {
     int i;
     if (argc > 2 && argv[1][0] == '-') {
+        parse_flag(argc, argv);
         i = 2;
     } else i = 1;
     for (; i < argc; i++) {
@@ -16,7 +17,7 @@ void print_file(const char* file_name) {
     char buff[100] = {};
     FILE* fp = fopen(file_name, "r");
     if (fp == NULL) {
-        perror("Ошибка открытия файла");
+        printf("cat: %s: No such file or directory", file_name);
         return;
     }
     while (fgets(buff, sizeof(buff), fp)) {
@@ -24,4 +25,19 @@ void print_file(const char* file_name) {
     }
     fclose(fp);
     return;
+}
+
+void parse_flag(int argc, const char** argv) {
+    char* use_flag = (char*)malloc(10 * sizeof(char*));
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') append_flag(argv[i], use_flag);
+    }
+    puts(use_flag);
+}
+
+void append_flag(const char* str, char* use_flag) {
+    for (int i = 0; i < count_flag; i++) {
+        if (!strcmp(str, opt[i].flags)) 
+            strcat(use_flag, opt[i].simple_flag);
+    }
 }
--- cat/s21_cat.h
+++ cat/s21_cat.h
@@ -2,20 +2,24 @@
 #define S21_CAT_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 void print_file(const char* file_name);
 void parse_flag();
 void flag_handler();
+void append_flag();
+
+enum {count_flag = 8};
 
 typedef struct {
     char* flags;
     char* simple_flag;
 } OPTIONS;
 
-OPTIONS opt[] = {
-    {"b", "b"}, {"e", "vE"}, {"n", "n"}, {"s", "s"}, {"t", "vT"},
-    {"number-nonblank", "b"}, {"number", "n"}, {"squeezee-blank", "s"}
+OPTIONS opt[count_flag] = {
+    {"-b", "b"}, {"-e", "vE"}, {"-n", "n"}, {"-s", "s"}, {"-t", "vT"},
+    {"-number-nonblank", "b"}, {"-number", "n"}, {"-squeezee-blank", "s"}
 };
 
 #endif
