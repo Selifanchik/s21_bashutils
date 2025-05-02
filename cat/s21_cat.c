35ddcfb40716247d71ada5b524165e39b5cc1425
Selifanchik <selifanchik@mail.ru>
2025-05-02 21:26:03 +0300

    Fixed a bug with remembering the previous char

--- cat/s21_cat.c
+++ cat/s21_cat.c
@@ -1,116 +1,130 @@
 #include "s21_cat.h"
 
 int main(int argc, char** argv) {
-    struct flags use_flag = {};
-    int flag_error = 0;
-    int* arg_ind = (int*)calloc(argc - 1, sizeof(int));
-    int ind = 0;
-    if (arg_ind == 0) {
+  struct flags use_flag = {};
+  int flag_error = 0;
+  int* arg_ind = (int*)calloc(argc - 1, sizeof(int));
+  int ind = 0;
+  if (arg_ind == 0) {
+    flag_error = 1;
+    printf("Memory allocation error");
+  }
+  if (parse_string(argc, argv, &use_flag, arg_ind, &ind) == 0)
+    print_file(argv, &use_flag, arg_ind, &ind);
+  free(arg_ind);
+  return flag_error;
+}
+
+int parse_string(int argc, char** argv, struct flags* use_flag, int* arg_index,
+                 int* ind) {
+  int opt, flag_error = 0;
+  while ((opt = getopt_long(argc, argv, "-setETbn", long_options, NULL)) !=
+             -1 &&
+         !flag_error) {
+    switch (opt) {
+      case 's':
+        use_flag->flag_s = 1;
+        break;
+      case 'e':
+        use_flag->flag_e = 1;
+        use_flag->flag_v = 1;
+        break;
+      case 'E':
+        use_flag->flag_e = 1;
+        break;
+      case 't':
+        use_flag->flag_t = 1;
+        use_flag->flag_v = 1;
+        break;
+      case 'T':
+        use_flag->flag_t = 1;
+        break;
+      case 'b':
+        use_flag->flag_b = 1;
+        use_flag->flag_n = 0;
+        break;
+      case 'n':
+        if (use_flag->flag_b == 0) use_flag->flag_n = 1;
+        break;
+      case '?':
+        opterr = 0;
         flag_error = 1;
-        printf("Memory allocation error");
-    }
-    if (parse_string(argc, argv, &use_flag, arg_ind, &ind) == 0)
-        print_file(argv, &use_flag, arg_ind, &ind);
-    free(arg_ind);
-    return flag_error;
-}
-
-int parse_string(int argc, char** argv, struct flags* use_flag, int* arg_index, int* ind) {
-    int opt, flag_error = 0;
-    while ((opt = getopt_long(argc, argv, "-setETbn", long_options, NULL)) != -1 && !flag_error) {
-        switch (opt) {
-        case 's':
-            use_flag->flag_s = 1;
-            break;
-        case 'e':
-            use_flag->flag_e = 1;
-            use_flag->flag_v = 1;
-            break;
-        case 'E':
-            use_flag->flag_e = 1;
-            break;
-        case 't':
-            use_flag->flag_t = 1;
-            use_flag->flag_v = 1;
-            break;
-        case 'T':
-            use_flag->flag_t = 1;
-            break;
-        case 'b':
-            use_flag->flag_b = 1;
-            use_flag->flag_n = 0;
-            break;
-        case 'n':
-            if (use_flag->flag_b == 0) use_flag->flag_n = 1;
-            break;
-        case '?':
-            opterr = 0;
-            flag_error = 1;
-            break;
-        default:
-            if (optarg) arg_index[(*ind)++] = optind - 1;
-            else {
-                printf("Unknown error");
-                flag_error = 1;
-            }
-            break;
+        break;
+      default:
+        if (optarg)
+          arg_index[(*ind)++] = optind - 1;
+        else {
+          printf("Unknown error");
+          flag_error = 1;
         }
+        break;
     }
-    if (*ind == 0 && !flag_error) {
-        printf("No arguments");
-        flag_error = 1;
-    } 
-    return flag_error;
+  }
+  if (*ind == 0 && !flag_error) {
+    printf("No arguments");
+    flag_error = 1;
+  }
+  return flag_error;
 }
 
-void print_file(char** file_name, struct flags* use_flag, int* arg_ind, int* ind) {
-    int index, count_number = 0;
-    for (int i = 0; i < *ind; i++) {
-        index = arg_ind[i];
-        FILE* file_stream = fopen(file_name[index], "r");
-        if (!file_stream) {
-            printf("./s21_cat: %s: No such file or directory", file_name[index]);
-        } else {
-            process_file(file_stream, use_flag, &count_number);
-            fclose(file_stream);
-        }
+void print_file(char** file_name, struct flags* use_flag, int* arg_ind,
+                int* ind) {
+  int index, count_number = 0;
+  for (int i = 0; i < *ind; i++) {
+    index = arg_ind[i];
+    FILE* file_stream = fopen(file_name[index], "r");
+    if (!file_stream) {
+      printf("./s21_cat: %s: No such file or directory", file_name[index]);
+    } else {
+      process_file(file_stream, use_flag, &count_number);
+      fclose(file_stream);
     }
+  }
 }
 
-void process_file(FILE* file_stream, struct flags* use_flag, int* count_number) {
-    int symbol;
-    int count_empty_line = 0;
-    char previous_symbol = '\n';
-    int print_done = 0;
-    while ((symbol = fgetc(file_stream)) != EOF) {
-        print_done = 0;
-        if (symbol == '\n' && previous_symbol == '\n') count_empty_line++;
-        else count_empty_line = 0;
-        if (use_flag->flag_s && count_empty_line > 1) continue;
-        if (previous_symbol == '\n' && (use_flag->flag_n || (use_flag->flag_b && count_empty_line == 0))) 
-            printf("%6d\t", ++(*count_number));
-        if (use_flag->flag_e && symbol == '\n') printf("$");
-        if (use_flag->flag_t && symbol == '\t') {
-                printf("^I");
-                print_done = 1;
-            }
-        if (use_flag->flag_v && symbol != '\n' && symbol != '\t')
-                process_flag_v(&symbol, &print_done);
-        if (!print_done) {
-            printf("%c", symbol);
-            previous_symbol = symbol;
-        }
+void process_file(FILE* file_stream, struct flags* use_flag,
+                  int* count_number) {
+  int symbol;
+  int count_empty_line = 0;
+  char previous_symbol = '\n';
+  int print_done = 0;
+  while ((symbol = fgetc(file_stream)) != EOF) {
+    print_done = 0;
+    if (symbol == '\n' && previous_symbol == '\n')
+      count_empty_line++;
+    else
+      count_empty_line = 0;
+    if (use_flag->flag_s && count_empty_line > 1) continue;
+    if (previous_symbol == '\n' &&
+        (use_flag->flag_n || (use_flag->flag_b && count_empty_line == 0)))
+      printf("%6d\t", ++(*count_number));
+    if (use_flag->flag_e && symbol == '\n') {
+      printf("$");
+      print_done = 0;
     }
+    if (use_flag->flag_t && symbol == '\t') {
+      printf("^I");
+      print_done = 1;
+    }
+    if (use_flag->flag_v && symbol != '\n' && symbol != '\t' && symbol >= 0 &&
+        symbol <= 255)
+      show_nonpriting_char(&symbol, &print_done);
+    if (!print_done) {
+      printf("%c", symbol);
+    }
+    previous_symbol = symbol;
+  }
 }
 
-void process_flag_v(const int* symbol, int* print_done) {
-    if (*symbol >= 0 && *symbol <= 31)
-        printf("^%c", *symbol + 64);
-    if (*symbol > 127)
-        printf("M-%c", *symbol % 128);
-    if (*symbol == 127)
-        printf("^?");
-    if (*symbol >= 32 && *symbol <=126)
-        printf("%c", *symbol);
-    *print_done = 1;
+void show_nonpriting_char(const int* symbol, int* print_done) {
+  int new_symbol;
+  if (*symbol >= 128 && *symbol <= 255) {
+    printf("M-");
+    new_symbol = *symbol % 128;
+  } else
+    new_symbol = *symbol;
+  if (new_symbol >= 0 && new_symbol <= 31) printf("^%c", new_symbol + 64);
+  if (new_symbol >= 32 && new_symbol <= 126) printf("%c", new_symbol);
+  if (new_symbol == 127) printf("^?");
+  *print_done = 1;
 }
--- cat/s21_cat.h
+++ cat/s21_cat.h
@@ -1,11 +1,11 @@
 #ifndef S21_CAT_H
 #define S21_CAT_H
 
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h>
 
-enum {size_buffer = 256};
+enum { size_buffer = 256 };
 
 struct flags {
   int flag_s;
@@ -17,15 +17,16 @@ struct flags {
 };
 
 struct option long_options[] = {
-  {"number-nonblank", optional_argument, NULL, 'b'},
-  {"number", optional_argument, NULL, 'n'},
-  {"squeeze-blank", optional_argument, NULL, 's'},
-  {0, 0, 0, 0}
-};
+    {"number-nonblank", optional_argument, NULL, 'b'},
+    {"number", optional_argument, NULL, 'n'},
+    {"squeeze-blank", optional_argument, NULL, 's'},
+    {0, 0, 0, 0}};
 
-int parse_string(int argc, char** argv, struct flags* use_flag, int* arg_index, int* ind);
-void print_file(char** file_name, struct flags* use_flag, int* arg_ind, int* ind);
+int parse_string(int argc, char** argv, struct flags* use_flag, int* arg_index,
+                 int* ind);
+void print_file(char** file_name, struct flags* use_flag, int* arg_ind,
+                int* ind);
 void process_file(FILE* file_stream, struct flags* use_flag, int* count_number);
-void process_flag_v(const int* symbol, int* print_done);
+void show_nonpriting_char(const int* symbol, int* print_done);
 
 #endif
