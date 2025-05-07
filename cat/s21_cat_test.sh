fa23c4b56a970a71e842b90f3a659f31905f5692
Danelle Lavern <danellel@ne-e6.nn.21-school.ru>
2025-05-07 18:34:14 +0300

    add tests

--- cat/s21_cat_test.sh
+++ cat/s21_cat_test.sh
@@ -15,43 +15,54 @@ draw_box() {
     printf '+\n'
 }
 
+# Тесты с флагами
 for flag in $flags; do
-	name="Test flag $flag..."
-	draw_box "$name"
-	for file in "$my_way/test_files/"*; do
-		./s21_cat $flag $file > result_s21_cat.txt
-		cat $flag $file > result_cat.txt
-		if diff -q result_s21_cat.txt result_cat.txt >/dev/null; then
-			echo -e "✅ \e[36m$(basename "$file")\e[0m"
-		else 
-			echo -e "❌ \e[36m$(basename "$file")\e[0m"
-		fi
-	done
+    name="Test flag $flag..."
+    draw_box "$name"
+    for file in "$my_way/test_files/"*; do
+        ./s21_cat $flag $file > result_s21_cat.txt
+        cat $flag $file > result_cat.txt
+        if diff -q result_s21_cat.txt result_cat.txt >/dev/null; then
+            echo -e "✅ \e[36m$(basename "$file")\e[0m"
+        else 
+            echo -e "❌ \e[36m$(basename "$file")\e[0m"
+        fi
+    done
 done
 
 file2="test_files/ascii-art.txt"
 file1="test_files/simple.txt"
 
 for flag in $flags; do
-	name="Test flag $flag..."
-	draw_box "$name"
-	./s21_cat $flag $file1 $file2 > result_s21_cat.txt
-	cat $flag $file1 $file2 > result_cat.txt
-	if diff -q result_s21_cat.txt result_cat.txt >/dev/null; then
-		echo -e "✅ \e[36m$(basename "$file1") $(basename "$file2")\e[0m"
-	else 
-		echo -e "❌ \e[36m$(basename "$file1") $(basename "$file2")\e[0m"
-	fi
+    name="Test flag $flag with multiple files..."
+    draw_box "$name"
+    ./s21_cat $flag $file1 $file2 > result_s21_cat.txt
+    cat $flag $file1 $file2 > result_cat.txt
+    if diff -q result_s21_cat.txt result_cat.txt >/dev/null; then
+        echo -e "✅ \e[36m$(basename "$file1") $(basename "$file2")\e[0m"
+    else 
+        echo -e "❌ \e[36m$(basename "$file1") $(basename "$file2")\e[0m"
+    fi
 done
 
-name="Test flag -nnnsst -tE -be..."
-draw_box "$name"
+# Тест без флагов с одиночными файлами
+draw_box "Test without flags with single files..."
 for file in "$my_way/test_files/"*; do
-	./s21_cat -nnnsst $file1 -tE $file2 -be > result_s21_cat.txt
-	cat -nnnsst "$file1" -tE $file2 -be > result_cat.txt
-	if diff -q result_s21_cat.txt result_cat.txt >/dev/null; then
-		echo -e "✅ \e[36m$(basename "$file")\e[0m"
-	else 
-		echo -e "❌ \e[36m$(basename "$file")\e[0m"
-	fi
+    ./s21_cat "$file" > result_s21_cat.txt
+    cat "$file" > result_cat.txt
+    if diff -q result_s21_cat.txt result_cat.txt >/dev/null; then
+        echo -e "✅ \e[36m$(basename "$file")\e[0m"
+    else
+        echo -e "❌ \e[36m$(basename "$file")\e[0m"
+    fi
 done
+
+# Тест без флагов с несколькими файлами
+draw_box "Test without flags with multiple files..."
+./s21_cat "$file1" "$file2" > result_s21_cat.txt
+cat "$file1" "$file2" > result_cat.txt
+if diff -q result_s21_cat.txt result_cat.txt >/dev/null; then
+    echo -e "✅ \e[36m$(basename "$file1") $(basename "$file2")\e[0m"
+else
+    echo -e "❌ \e[36m$(basename "$file1") $(basename "$file2")\e[0m"
+fi
